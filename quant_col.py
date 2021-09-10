c5434efb624f605116d59dd14209e0dff23df959
Graculus-Green <37638074+Graculus-Green@users.noreply.github.com>
2021-09-10 12:05:42 +0100

    Add files via upload

---
+++ quant_col.py
@@ -0,0 +1,25 @@
+# To work in Jupyter
+# img is the directory path of the image you want to quantise
+# num is the number of different colours you want to use
+
+def quant_col(img, num):
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
+    from sklearn.cluster import KMeans
+
+    #image as array
+    image_as_array = mpimg.imread(img)
+    (h,w,c) = image_as_array.shape
+    image_as_2d = image_as_array.reshape(h*w,c)
+
+    #quantise colours
+    model = KMeans(n_clusters=num)
+    labels = model.fit_predict(image_as_2d)
+    rgb_codes = model.cluster_centers_.round(0).astype(int)
+    quantised_img = np.reshape(rgb_codes[labels], (h,w,c)) # reshape back to 3D
+
+    #display
+    plt.figure(dpi = 200)
+    plt.imshow(quantised_img)
